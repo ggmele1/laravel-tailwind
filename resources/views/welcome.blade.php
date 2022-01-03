b86bd4ce14302617434c2adf74c63723316da748
Giuseppe Mele <giuseppe@dovly.com>
2022-01-03 08:39:41 -0700

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+
+</head>
+
+<body>
+    <div class="w-full h-full bg-black">
+        <h1 class="text-red-500 text-xl">Hello world</h1>
+    </div>
+</body>
+
+</html>
